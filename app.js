1173f865f45e247eb769d8b8f6f409b5ea27a8b1
Leechiayu <gary86442@gmail.com>
2023-03-01 00:00:53 +0800

    feat:add incorrect user login

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ const port = 3000;
 
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/", (req, res) => {
+  const userKeyIn = req.body;
+  let user = users.filter(
+    (user) =>
+      user.email === userKeyIn.emil && user.password === userKeyIn.userPassword
+  );
+  if (user.length) {
+    res.render("welcome", { user });
+  } else {
+    const incorrect = "incorrect";
+    res.render("index", { incorrect });
+  }
 });
 
 app.listen(port, () => {
