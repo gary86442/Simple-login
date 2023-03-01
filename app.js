7281006c00e3be16654cdd054b04fe0a78ae848d
Leechiayu <gary86442@gmail.com>
2023-03-01 10:31:30 +0800

    feat: add login correct render welcome.hbs

--- app.js
+++ app.js
@@ -7,24 +7,30 @@ const port = 3000;
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
+
+//首頁
 app.get("/", (req, res) => {
   res.render("index");
 });
-
+//處理登入提交
 app.post("/", (req, res) => {
   const userKeyIn = req.body;
-  let user = users.filter(
+  //驗證身分
+  let user = users.find(
     (user) =>
-      user.email === userKeyIn.emil && user.password === userKeyIn.userPassword
+      user.email === userKeyIn.userEmail &&
+      user.password === userKeyIn.userPassword
   );
-  if (user.length) {
+  //成功登入
+  if (user) {
     res.render("welcome", { user });
   } else {
+    //失敗回首頁 顯示錯誤
     const incorrect = "incorrect";
     res.render("index", { incorrect });
   }
 });
-
+// 監聽伺服器
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 });
---
+++ try.js
@@ -0,0 +1,3 @@
+let userEmail = "tony@stark.com";
+let email = "tony@stark.com";
+console.log(userEmail === email);
